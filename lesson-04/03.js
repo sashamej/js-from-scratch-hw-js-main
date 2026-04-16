f7eb80c43a14218dd9b49ff52e88765dec853895
sashamej <alexmej2023@gmail.com>
2026-04-16 11:49:29 +0300

    complete 04/03

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -19,4 +19,19 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements (array1, array2) {
+	const result = [];
+	
+	for (let i = 0; i < array1.length; i ++) {
+		const current = array1[i];
+		
+		if (array2.includes (current) && !result.includes (current)) {
+			result.push (current);
+		}
+	}
+	return result;
+}
+
+//const array1 = [1, 2, 3];
+//const array2 = [2, 3, 4];
+//console.log (findCommonElements (array1, array2));
